2c0bb4aa9d7a7f9ca676d3538a112c13129fb505
prehensile <me@prehensile.co.uk>
2025-10-03 13:15:29 +0100

    added wrapper  page for html format cv

--- cv/build.sh
+++ cv/build.sh
@@ -8,9 +8,9 @@ md_timestamp=$(git log -1 --format=%ct cv.md)
 if [ $md_timestamp -gt $pdf_timestamp ]; then
     echo "Generating new PDF..."
     npm ci
-    npm run build
-    git add cv.pdf
-    git commit -m "github actions generated PDF on $(date +%Y-%m-%dT%H:%M:%S)"
+    npm run build -- --debug
+    git add cv.pdf cv.html
+    git commit -m "github actions generated PDF and HTML on $(date +%Y-%m-%dT%H:%M:%S)"
     git push origin HEAD:master
 else
     echo "cv.pdf is up to date with cv.md, no action needed"
